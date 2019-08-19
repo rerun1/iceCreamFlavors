1ab705848348239853a3639e3c56d0b6f7ade119
Suzi Rubino <suzi@suzirubi.com>
2019-08-19 15:58:34 -0700

    refactored script with for loops, now are within the user logic, working on getting them up into business

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,9 @@
 var flavors = ["Bacon", "Bastani Sonnati", "Beer", "Birthday Cake", "Black Sesame Seed", "Blue Moon", "Brown Bread", "Butter Brickle", "Butter Pecan", "Candy Bar", "Cherry", "Chocolate", "Chocolate Chip", "Chocolate Chip Cookie Dough", "Coconut", "Cookie Dough", "Cookies and Cream", "Cotton Candy", "Crab", "Creme de Menthe", "Dulce de Leche", "Durian", "Egg Custard", "French Vanilla", "Garlic", "Grape", "Grape-Nuts", "Green Tea", "Halva", "Heath Bar", "Hokey Pokey", "Honeycomb Toffee", "Lucuma", "Mamey", "Mango", "Marshmallow creme", "Mint", "Mint Chocolate Chip", "Moon Mist", "Moose Tracks", "Neapolitan", "Nuts", "Oyster", "Peppermint", "Pistachio", "Raspberry Ripple", "Rocky Road", "Spearmint", "Spumoni", "Stracciatella", "Strawberry", "Superman", "Teaberry", "Tiger Tail", "Toffee", "Tutti Frutti", "Ube", "Vanilla", "Wintergreen"];
 
-console.log(flavors);
+var americasTopTen = ["Vanilla", "Chocolate", "Cookies and Cream", "Mint Chocolate Chip", "Chocolate Chip Cookie Dough", "Butter Pecan", "Cookie Dough", "Strawberry", "Moose Tracks", "Neapolitan"];
 
-var topTenFlavors = [];
+var myTopTen = ["Butter Pecan", "Cherry", "Chocolate", "Spumoni", "Rocky Road", "Mango", "French Vanilla", "Pistachio", "Cookies and Cream", "Rum Raisun"];
 
-topTenFlavors.push(flavors[57]);
-topTenFlavors.push(flavors[11]);
-topTenFlavors.push(flavors[16]);
-topTenFlavors.push(flavors[37]);
-topTenFlavors.push(flavors[13]);
-topTenFlavors.push(flavors[8]);
-topTenFlavors.push(flavors[15]);
-topTenFlavors.push(flavors[50]);
-topTenFlavors.push(flavors[39]);
-topTenFlavors.push(flavors[40]);
-
-console.log(topTenFlavors);
-
-var myTopTenFlavors = [];
-
-myTopTenFlavors.push(flavors[8]);
-myTopTenFlavors.push(flavors[10]);
-myTopTenFlavors.push(flavors[11]);
-myTopTenFlavors.push(flavors[48]);
-myTopTenFlavors.push(flavors[46]);
-myTopTenFlavors.push(flavors[34]);
-myTopTenFlavors.push(flavors[23]);
-myTopTenFlavors.push(flavors[44]);
-myTopTenFlavors.push(flavors[16]);
-myTopTenFlavors.push("Rum Raisun");
-
-console.log(myTopTenFlavors);
 
 
 $(document).ready(function(){
@@ -39,14 +12,13 @@ $(document).ready(function(){
   $("ol#myTopTen").empty();
   $("ol#wiki").empty();
 
-  topTenFlavors.forEach(function(flavor){
-    $("ol#americasTopTen").append("<li>"+flavor+"</li>");
-  });
-  myTopTenFlavors.forEach(function(flavor){
-    $("ol#myTopTen").append("<li>"+flavor+"</li>");
-  });
-  flavors.forEach(function(flavor){
-    $("ol#wiki").append("<li>"+flavor+"</li>");
-  });
-
+  for (var flavor = 0; flavor < americasTopTen.length; flavor += 1) {
+    $("ol#americasTopTen").append("<li>"+americasTopTen[flavor]+"</li>");
+  }
+  for (var flavor = 0; flavor < myTopTen.length; flavor += 1) {
+    $("ol#myTopTen").append("<li>"+myTopTen[flavor]+"</li>");
+  }
+  for (var flavor = 0; flavor < flavors.length; flavor += 1) {
+    $("ol#wiki").append("<li>"+flavors[flavor]+"</li>")
+  }
 });
